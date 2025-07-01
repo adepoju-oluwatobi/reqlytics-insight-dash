27c1c02d1d7b0cfe515c88f9d56207036e05ead9
Adepoju Oluwatobi <kunletobi4@gmail.com>
2025-07-01 07:31:00 +0100

    rmd .env

--- src/base_url.tsx
+++ src/base_url.tsx
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  import.meta.env.MODE === "production"
+  import.meta.env.MODE === "development"
     ? import.meta.env.VITE_DEVELOPMENT_ENDPOINT
     : import.meta.env.VITE_PRODUCTION_ENDPOINT;
